dad261aed12a1f9a8de5b57fc1d9d3618a4e9451
dbeja <david.beja@gmail.com>
2021-03-31 23:03:31 +0100

    Show modal on keyup

--- assets/wipi.js
+++ assets/wipi.js
@@ -1,3 +1,19 @@
 import 'alpinejs'
 
-console.log('HELLO WIPI!!!');
+function wipi() {
+	return {
+		modal: false,
+
+		init() {
+			const self = this;
+			document.addEventListener('keyup', function(e) {
+				if (e.ctrlKey && e.key === ' ') {
+					console.log('SPACE!!!');
+					self.modal = !self.modal;
+				}
+			}, false);
+		}
+	}
+}
+
+window.wipi = wipi;
--- includes/class-wipi.php
+++ includes/class-wipi.php
@@ -27,9 +27,19 @@ class Wipi {
 	public function setup() {
 		// enqueue admin scripts.
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts' ) );
+
+		// add wipi modal to admin.
+		add_action( 'admin_footer', array( $this, 'modal_html' ) );
 	}
 
 	public function admin_scripts() {
 		wp_enqueue_script( 'wipi-js', WIPI_PLUGIN_URL . '/assets/dist/wipi.js' );
+	}
+
+	public function modal_html() {
+		?>
+		<div id="wipi-modal" x-data="wipi()" x-init="init" x-show="modal" style="position: fixed; top: 50%; left: 50%; width: 300px; height: 100px; background-color: yellow; display: none;">
+		</div>
+		<?php
 	}
 }
