477e3accbb6cd888e2b5148d737fc44ab13b0f79
dbeja <david.beja@gmail.com>
2021-04-07 16:47:58 +0100

    Close modal with ESC and mouse click outside

--- src/wipi.js
+++ src/wipi.js
@@ -8,8 +8,11 @@ function wipi() {
 
 		init(nextTick) {
 			const self = this;
+
+			// Hotkeys.
 			document.addEventListener('keyup', function(e) {
-				if ( e.ctrlKey && e.key === ' ' ) {
+				// Toggle modal.
+				if ( e.ctrlKey && ' ' === e.key ) {
 					self.modal = !self.modal;
 
 					if ( self.modal ) {
@@ -17,6 +20,18 @@ function wipi() {
 							document.getElementById('wipi-modal-input').focus();
 						});
 					}
+				}
+
+				// Esc - close modal.
+				if ( 'Escape' === e.key ) {
+					self.modal = false;
+				}
+			}, false);
+
+			// Mouse click outside Wipi to close it.
+			document.addEventListener('click', function(e) {
+				if ( ! document.getElementById('wipi-modal').contains(e.target) ) {
+					self.modal = false;
 				}
 			}, false);
 		},
